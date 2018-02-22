77bfc3dc524cb0f68a9a4468e59e526ba75a07f0
jiraphaty <jiraphattar3@gmail.com>
2018-02-22 18:57:45 +0700

    Add files via upload

---
+++ openvpn14.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+
+#install openvpn
+
+apt-get update
+apt-get install openvpn easy-rsa
+
+wget -O /etc/openvpn/openvpn.tar "https://raw.githubusercontent.com/jiraphaty/tar-vpn/master/openvpn.tar"
+wget -O /etc/openvpn/default.tar "https://raw.githubusercontent.com/jiraphaty/tar-vpn/master/default.tar"
+cd /etc/openvpn/
+tar xf openvpn.tar
+tar xf default.tar
+cp sysctl.conf /etc/
+cp before.rules /etc/ufw/
+cp ufw /etc/default/
+service openvpn start
+service openvpn restart
