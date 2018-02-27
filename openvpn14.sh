5b8be8e241e024e06e500062c2eb54e368d5c1fb
jiraphaty <jiraphattar3@gmail.com>
2018-02-27 18:34:27 +0700

    Update openvpn14.sh

--- openvpn14.sh
+++ openvpn14.sh
@@ -54,9 +54,35 @@ ufw allow 8080/tcp
 ufw allow 3128/tcp
 ufw allow 80/tcp
 yes | sudo ufw enable
+# download script
+cd /usr/bin
+wget -O menu "https://raw.github.com/blazevpn/autoscript/master/menu.sh"
+wget -O usernew "https://raw.github.com/blazevpn/autoscript/master/usernew.sh"
+wget -O trial "https://raw.github.com/blazevpn/autoscript/master/trial.sh"
+wget -O hapus "https://raw.github.com/blazevpn/autoscript/master/hapus.sh"
+wget -O login "https://raw.github.com/blazevpn/autoscript/master/user-login.sh"
+wget -O member "https://raw.github.com/blazevpn/autoscript/master/user-list.sh"
+wget -O resvis "https://raw.github.com/blazevpn/autoscript/master/resvis.sh"
+wget -O speedtest "https://raw.github.com/blazevpn/autoscript/master/speedtest_cli.py"
+wget -O about "https://raw.github.com/blazevpn/autoscript/master/about.sh"
+echo "0 0 * * * root /usr/bin/reboot" > /etc/cron.d/reboot
+echo "* * * * * service dropbear restart" > /etc/cron.d/dropbear
+chmod +x menu
+chmod +x usernew
+chmod +x trial
+chmod +x hapus
+chmod +x login
+chmod +x member
+chmod +x resvis
+chmod +x speedtest
+chmod +x about
 clear
 printf '###############################\n'
 printf '# Script by Jiraphat Yuenting #\n'
+printf '###############################\n'
+
+printf '###############################\n'
+printf '#    พิมพ์ menu เพื่อใช้คำสั่งต่างๆ   #\n'
 printf '###############################\n'
 
 printf '\n\nเพิ่ม user โดยใช้คำสั่ง useradd'
